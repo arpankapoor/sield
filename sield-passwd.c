53b642ea28171945159d1eb142b3b6b1f59044e2
Arpan Kapoor <k.arpan26@gmail.com>
2014-06-10 02:11:18 +0530

    Check password correctness.

--- sield-passwd.c
+++ sield-passwd.c
@@ -1,8 +1,9 @@
-#define _GNU_SOURCE		/* getline(), strdup() */
+#define _GNU_SOURCE		/* getline(), strdup(), crypt() */
 #define _BSD_SOURCE		/* strsep() */
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
+#include <unistd.h>		/* crypt() */
 
 static const char *shadow_file = "/etc/shadow";
 
@@ -18,7 +19,7 @@ static char *get_encrypted_user_passwd(const char *user)
 		return NULL;
 	}
 
-	const char colon = ":";
+	const char *colon = ":";
 	char *encrypted_passwd_copy = NULL;
 	char *line = NULL;
 	size_t len = 0;
@@ -42,9 +43,37 @@ static char *get_encrypted_user_passwd(const char *user)
 		len = 0;
 	}
 
-	if (! encrypted_password_copy)
+	if (! encrypted_passwd_copy)
 		log_fn("Unable to find %s's encrypted password in shadow file.", user);
 
 	fclose(shadow_fp);
 	return encrypted_passwd_copy;
+}
+
+/*
+ * Return 1 if given user's password matches with
+ * the one stored in the shadow file,
+ * otherwise return 0.
+ */
+int check_passwd(const char *user, const char *passwd)
+{
+	char *actual_encrypted_user_passwd = get_encrypted_user_passwd(user);
+
+	/*
+	 * salt is a character string starting with the characters "$id$"
+	 * followed by a string terminated by "$":
+	 *
+	 * 	$id$salt$encrypted
+	 *
+	 * Here actual encrypted password can act as salt.
+	 */
+	char *salt = actual_encrypted_user_passwd;
+	char *encrypted_passwd = crypt(passwd, salt);
+
+	int match = ! strcmp(actual_encrypted_user_passwd, encrypted_passwd);
+
+	free(actual_encrypted_user_passwd);
+	free(encrypted_passwd);
+
+	return match;
 }
