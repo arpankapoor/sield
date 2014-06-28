2e25e9fa734ae4c5555a8740aaf14a969bc5d4e5
Arpan Kapoor <k.arpan26@gmail.com>
2014-06-28 12:28:00 +0530

    GUI is now written in GTK2.

--- Makefile
+++ Makefile
@@ -2,20 +2,19 @@ CC=gcc
 LUDEV=-ludev
 LCRYPT=-lcrypt
 CFLAGS=-Wall
-GTK_CFLAGS=`pkg-config --cflags gtk+-3.0`
-GTK_LDFLAGS=`pkg-config --libs gtk+-3.0` -rdynamic
-CLAMAV=-lclamav
+GTK_CFLAGS=`pkg-config --cflags gtk+-2.0`
+GTK_LDFLAGS=`pkg-config --libs gtk+-2.0` -rdynamic
 
 all: sield passwd-sield
 
 sield: sield.o sield-av.o sield-config.o sield-daemon.o sield-log.o sield-mount.o \
-	sield-passwd-check.o sield-passwd-dialog.o sield-udev-helper.o
-	$(CC) $(CFLAGS) $(LUDEV) $(LCRYPT) $(GTK_LDFLAGS) $(CLAMAV) -o $@ $^
+	sield-passwd-check.o sield-passwd-dialog-gtk2.o sield-udev-helper.o
+	$(CC) $(CFLAGS) $(LUDEV) $(LCRYPT) $(GTK_LDFLAGS) -o $@ $^
 
 passwd-sield: sield-config.o sield-log.o sield-passwd-update.o sield-passwd-check.o
 	$(CC) $(CFLAGS) $(LUDEV) $(LCRYPT) -o $@ $^
 
-sield-passwd-dialog.o: sield-passwd-dialog.c
+sield-passwd-dialog-gtk2.o: sield-passwd-dialog-gtk2.c
 	$(CC) $(CFLAGS) $(GTK_CFLAGS) -c -o $@ $^
 
 install:
--- sield-passwd-dialog.c
+++ sield-passwd-dialog.c
@@ -1,49 +1,19 @@
 #define _GNU_SOURCE
-#include <stdio.h>
+#include <gtk/gtk.h>
 #include <stdlib.h>
 #include <string.h>
-#include <gtk/gtk.h>
 
+#include "sield-log.h"
 #include "sield-passwd-check.h"
 #include "sield-passwd-dialog.h"
 
-static const gchar *glade_file = "sield.glade";
+struct passwd_data {
+	GtkWidget *entry;
+	GtkWidget *wrong_passwd;
+};
+
 static int passwd_correct;
-void on_ok_button_clicked(GtkWidget *ok_button, gpointer *data);
-void on_cancel_button_clicked(GtkWidget *cancel_button, gpointer *data);
-int ask_passwd_dialog(const char *manufacturer,
-	       const char *product);
-
-void on_ok_button_clicked(GtkWidget *ok_button, gpointer *data)
-{
-	GObject *dialog = G_OBJECT(data);
-
-	GtkLabel *wrong_passwd_label = g_object_get_data(
-					dialog, "wrong_passwd_label");
-
-	GtkEntry *passwd_entry = g_object_get_data(dialog, "passwd_entry");
-	const gchar *plain_txt_passwd = gtk_entry_get_text(passwd_entry);
-
-	if (passwd_check(plain_txt_passwd)) {
-		passwd_correct = 1;
-		gtk_widget_destroy(GTK_WIDGET(dialog));
-		gtk_main_quit();
-	} else {
-		/* Clear the password entry text area */
-		gtk_entry_set_text(passwd_entry, "");
-		gtk_widget_show(GTK_WIDGET(wrong_passwd_label));
-	}
-}
-
-void on_cancel_button_clicked(GtkWidget *cancel_button, gpointer *data)
-{
-	GObject *dialog = G_OBJECT(data);
-
-	passwd_correct = 0;
-
-	gtk_widget_destroy(GTK_WIDGET(dialog));
-	gtk_main_quit();
-}
+static void passwd_response(GtkWidget *widget, int response, gpointer data);
 
 /*
  * Display the password input dialog.
@@ -52,49 +22,124 @@ void on_cancel_button_clicked(GtkWidget *cancel_button, gpointer *data)
  * return 0.
  */
 int ask_passwd_dialog(const char *manufacturer,
-	       const char *product)
+		const char *product)
 {
 	gtk_init(0, NULL);
 
-	GtkBuilder *builder = gtk_builder_new_from_file(glade_file);
+	GtkWidget *dialog = gtk_dialog_new();
 
-	GtkWidget *dialog = GTK_WIDGET(gtk_builder_get_object(
-				builder, "dialog"));
+	/* Cancel & OK buttons */
+	gtk_dialog_add_button(GTK_DIALOG(dialog),
+		GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL);
+	gtk_dialog_add_button(GTK_DIALOG(dialog),
+		GTK_STOCK_OK, GTK_RESPONSE_OK);
 
-	GtkLabel *device_info_label = GTK_LABEL(gtk_builder_get_object(
-					builder, "device_info_label"));
+	/* Authentication image */
+	GtkWidget *image = gtk_image_new_from_stock(
+		GTK_STOCK_DIALOG_AUTHENTICATION, GTK_ICON_SIZE_DIALOG);
 
-	/* Print device information in the dialog. */
+	/* Default response is OK */
+	gtk_dialog_set_default_response(GTK_DIALOG(dialog),
+					GTK_RESPONSE_OK);
+
+	GtkWidget *hbox = gtk_hbox_new(FALSE, 5);
+	gtk_box_pack_start(GTK_BOX(hbox), image, FALSE, FALSE, 12);
+
+	/* Print device info */
 	char *dev_info = NULL;
 	if (asprintf(&dev_info, "%s %s inserted.\n"
 			"Authorization needed to mount and share.",
 			manufacturer, product) == -1) {
 		dev_info = strdup("USB block device inserted.\n"
-				"Authorization needed to mount and share.\n");
+				"Authorization needed to mount and share.");
 	}
-	gtk_label_set_text(device_info_label, dev_info);
-	if (dev_info) free (dev_info);
 
-	/*
-	 * Pass passwd_entry and wrong_passwd_label to the "OK"
-	 * button on being clicked.
-	 */
-	GtkEntry *passwd_entry = GTK_ENTRY(gtk_builder_get_object(
-				builder, "passwd_entry"));
-	GtkLabel *wrong_passwd_label = GTK_LABEL(gtk_builder_get_object(
-				builder, "wrong_passwd_label"));
+	GtkWidget *label = gtk_label_new(dev_info);
+	if (dev_info) free(dev_info);
 
-	g_object_set_data(G_OBJECT(dialog), "passwd_entry", passwd_entry);
-	g_object_set_data(G_OBJECT(dialog), "wrong_passwd_label", wrong_passwd_label);
+	gtk_box_pack_start(GTK_BOX(hbox), label, FALSE, FALSE, 12);
+	gtk_box_pack_start(GTK_BOX(gtk_dialog_get_content_area(GTK_DIALOG(dialog))),
+			hbox, FALSE, TRUE, 5);
 
-	gtk_builder_connect_signals(builder, dialog);
+	/* Password label and entry */
+	GtkWidget *vbox_labels = gtk_vbox_new(FALSE, 5);
+	GtkWidget *vbox_entries = gtk_vbox_new(FALSE, 5);
 
-	g_object_unref(G_OBJECT(builder));
+	hbox = gtk_hbox_new(FALSE, 5);
+	gtk_box_pack_start(GTK_BOX(gtk_dialog_get_content_area(GTK_DIALOG(dialog))),
+		hbox, FALSE, TRUE, 5);
 
-	/* Assume password is incorrect */
-	passwd_correct = 0;
+	gtk_box_pack_start(GTK_BOX(hbox), vbox_labels, FALSE, TRUE, 12);
+	gtk_box_pack_start(GTK_BOX(hbox), vbox_entries, TRUE, TRUE, 12);
+
+	GtkWidget *align = gtk_alignment_new(0.0, 0.5, 0.0, 0.0);
+	label = gtk_label_new("Password:");
+	gtk_container_add(GTK_CONTAINER(align), label);
+
+	gtk_box_pack_start(GTK_BOX(vbox_labels), align, TRUE, FALSE, 12);
+
+	struct passwd_data *password_data = malloc(sizeof password_data);
+	password_data->entry = gtk_entry_new();
+	gtk_entry_set_visibility(GTK_ENTRY(password_data->entry), FALSE);
+	gtk_entry_set_activates_default(GTK_ENTRY(password_data->entry),
+				TRUE);
+
+	gtk_box_pack_start(GTK_BOX(vbox_entries),
+			password_data->entry, TRUE, TRUE, 12);
+
+	/* Wrong password label. */
+	password_data->wrong_passwd = hbox = gtk_hbox_new(FALSE, 5);
+
+	image = gtk_image_new_from_stock(
+		GTK_STOCK_DIALOG_ERROR, GTK_ICON_SIZE_BUTTON);
+	gtk_box_pack_start(GTK_BOX(hbox), image, FALSE, FALSE, 12);
+
+	label = gtk_label_new("Incorrect password. Please try again.");
+	gtk_box_pack_start(GTK_BOX(hbox), label, FALSE, FALSE, 12);
+	gtk_box_pack_start(GTK_BOX(gtk_dialog_get_content_area(GTK_DIALOG(dialog))),
+			hbox, FALSE, TRUE, 5);
+
+	/* Hide wrong password label. */
+	gtk_widget_set_no_show_all(hbox, TRUE);
+
+	g_signal_connect(G_OBJECT(dialog), "response",
+			G_CALLBACK(passwd_response), password_data);
+
+	gtk_widget_show_all(GTK_WIDGET(gtk_dialog_get_content_area(GTK_DIALOG(dialog))));
 	gtk_widget_show(dialog);
+
+	passwd_correct = 0;
 	gtk_main();
 
+	free(password_data);
 	return passwd_correct;
+}
+
+static void passwd_response(GtkWidget *widget, int response, gpointer data)
+{
+	struct passwd_data *password_data = (struct passwd_data *) data;
+	const gchar *plain_txt_passwd = gtk_entry_get_text(GTK_ENTRY(password_data->entry));
+	switch (response) {
+	case GTK_RESPONSE_OK:
+		if (passwd_check(plain_txt_passwd)) {
+			passwd_correct = 1;
+			gtk_widget_destroy(widget);
+			gtk_main_quit();
+		} else {
+			passwd_correct = 0;
+
+			/* Clear password entry text area */
+			gtk_entry_set_text(GTK_ENTRY(password_data->entry), "");
+
+			/* Show incorrect password label */
+			gtk_widget_set_no_show_all(password_data->wrong_passwd, FALSE);
+			gtk_widget_show_all(password_data->wrong_passwd);
+			gtk_widget_show(password_data->wrong_passwd);
+		}
+		break;
+	default:
+		gtk_widget_destroy(widget);
+		gtk_main_quit();
+		break;
+	}
 }
