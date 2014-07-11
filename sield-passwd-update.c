d8f941f59d58dcd8d6dab008d65b4c9cbdbbb4fa
Arpan Kapoor <k.arpan26@gmail.com>
2014-07-12 04:29:04 +0530

    tabs -> spaces

--- sield-passwd-update.c
+++ sield-passwd-update.c
@@ -1,15 +1,14 @@
-#define _GNU_SOURCE		/* getline(), crypt() */
-#include <errno.h>
-#include <stdio.h>
-#include <stdlib.h>		/* rand(), srand(), free() */
-#include <string.h>		/* strcmp() */
-#include <termios.h>		/* tcgetattr(), tcsetattr() */
-#include <time.h>		/* time() */
-#include <unistd.h>		/* crypt() */
+#define _GNU_SOURCE     /* getline(), crypt() */
+#include <errno.h>      /* errno */
+#include <stdio.h>      /* fopen() */
+#include <stdlib.h>     /* rand(), srand(), free() */
+#include <string.h>     /* strcmp() */
+#include <termios.h>    /* tcgetattr(), tcsetattr() */
+#include <time.h>       /* time() */
+#include <unistd.h>     /* crypt() */
 
-#include "sield-config.h"	/* set-sield-attr() */
-#include "sield-log.h"		/* log_fn() */
-#include "sield-passwd-check.h"	/* passwd_check() */
+#include "sield-log.h"          /* log_fn() */
+#include "sield-passwd-check.h" /* is_passwd_correct() */
 
 static const char *PASSWD_FILE = "/etc/sield.passwd";
 
@@ -26,38 +25,38 @@ static int set_passwd(const char *plain_txt_passwd);
  */
 static ssize_t get_passwd(char **plain_txt_passwd, size_t *n, FILE *stream)
 {
-	struct termios old, new;
+    struct termios old, new;
+    ssize_t read = 0;
 
-	/* Get old terminal attributes. */
-	if (tcgetattr(fileno(stream), &old) == -1) {
-		fprintf(stderr,"Unable to GET terminal attributes.");
-		log_fn("Unable to GET terminal attributes.");
-		return -1;
-	}
+    /* Get old terminal attributes. */
+    if (tcgetattr(fileno(stream), &old) == -1) {
+        fprintf(stderr, "Unable to GET terminal attributes.");
+        log_fn("Unable to GET terminal attributes.");
+        return -1;
+    }
 
-	new = old;
-	new.c_lflag &= ~ECHO;	/* Turn echoing off. */
+    new = old;
+    new.c_lflag &= ~ECHO;	/* Turn echoing off. */
 
-	if (tcsetattr(fileno(stream), TCSAFLUSH, &new) == -1) {
-		fprintf(stderr, "Unable to SET terminal attributes.");
-		log_fn ("Unable to SET terminal attributes.");
-		return -1;
-	}
+    if (tcsetattr(fileno(stream), TCSAFLUSH, &new) == -1) {
+        fprintf(stderr, "Unable to SET terminal attributes.");
+        log_fn ("Unable to SET terminal attributes.");
+        return -1;
+    }
 
-	/* Read password */
-	ssize_t read;
-	read = getline(plain_txt_passwd, n, stream);
+    /* Read password */
+    read = getline(plain_txt_passwd, n, stream);
 
-	/* Restore terminal settings. */
-	(void) tcsetattr(fileno(stream), TCSAFLUSH, &old);
+    /* Restore terminal settings. */
+    (void) tcsetattr(fileno(stream), TCSAFLUSH, &old);
 
-	/* Change newline to '\0'. */
-	if (read > 0) {
-		char *passwd = *plain_txt_passwd;
-		passwd[read - 1] = '\0';
-	}
+    /* Change newline to '\0'. */
+    if (read > 0) {
+        char *passwd = *plain_txt_passwd;
+        passwd[read - 1] = '\0';
+    }
 
-	return read - 1;
+    return read > 0 ? (read - 1) : read;
 }
 
 /*
@@ -65,27 +64,36 @@ static ssize_t get_passwd(char **plain_txt_passwd, size_t *n, FILE *stream)
  */
 static char *generate_salt(void)
 {
-	char *salt = calloc(21, sizeof(char));
-	/* salt = "$6$random_string$" */
-	int idx = 0;
-	salt[idx++] = '$';
-	salt[idx++] = '6';
-	salt[idx++] = '$';
+    /* As defined in crypt(3). */
+    const char set[] =
+        "abcdefghijklmnopqrstuvwxyz"
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "0123456789"
+        "./";
+    const int SALT_LEN = 16;
 
-	/* As defined in crypt(3). */
-	static const char set[] =
-		"abcdefghijklmnopqrstuvwxyz"
-		"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-		"0123456789"
-		"./";
+    char *salt = NULL;
+    int idx = 0;
+    int i, size;
 
-	int i, size = strlen(set);
-	srand(time(NULL));
-	for (i = 0; i < 16; i++)
-		salt[idx++] = set[rand() / (RAND_MAX / size + 1)];
-	salt[idx] = '$';
-	salt[idx] = '\0';
-	return salt;
+    salt = calloc(SALT_LEN + 5, sizeof(char));
+
+    /* salt = "$6$random_string$" */
+    salt[idx++] = '$';
+    salt[idx++] = '6';
+    salt[idx++] = '$';
+
+    size = strlen(set);
+
+    srand(time(NULL));
+
+    for (i = 0; i < SALT_LEN; i++)
+        salt[idx++] = set[rand() / (RAND_MAX / size + 1)];
+
+    salt[idx++] = '$';
+    salt[idx] = '\0';
+
+    return salt;
 }
 
 /*
@@ -93,112 +101,111 @@ static char *generate_salt(void)
  *
  * Encrypt using SHA-512 with a 16 byte salt.
  *
- * Return 1 on success, else return 0.
+ * Return 0 on success, else return -1.
  */
 static int set_passwd(const char *plain_txt_passwd)
 {
-	/* Generate a salt. */
-	char *salt = generate_salt();
-	char *encrypted_passwd = crypt(plain_txt_passwd, salt);
+    char *salt = NULL;
+    char *encrypted_passwd = NULL;
+    FILE *passwd_fp = NULL;
 
-	FILE *passwd_fp = fopen(PASSWD_FILE, "w");
-	if (! passwd_fp) {
-		log_fn("Failed to open password file (%s): %s",
-			PASSWD_FILE, strerror(errno));
-		return 0;
-	}
+    /* Generate a salt. */
+    salt = generate_salt();
+    encrypted_passwd = crypt(plain_txt_passwd, salt);
 
-	if (fprintf(passwd_fp, "%s\n", encrypted_passwd) < 0) {
-		log_fn("Failed to write new password to %s", PASSWD_FILE);
-		return 0;
-	}
+    passwd_fp = fopen(PASSWD_FILE, "w");
+    if (passwd_fp == NULL) {
+        log_fn("fopen(): %s", strerror(errno));
+        log_fn("Failed to open password file \"%s\"", PASSWD_FILE);
+        return -1;
+    }
 
-	fclose(passwd_fp);
-	free(salt);
-	/*
-	 * DON'T FREE RETURN VALUE OF crypt(3).
-	 *
-	 * free(encrypted_passwd);
-	 */
+    fprintf(passwd_fp, "%s\n", encrypted_passwd);
 
+    if (salt) free(salt);
+    fclose(passwd_fp);
+    /*
+     * DON'T FREE RETURN VALUE OF crypt(3).
+     *
+     * free(encrypted_passwd);
+     */
 
-	/*
-	 * Password file should be readable
-	 * and writable only by the superuser.
-	 */
-	if (chown(PASSWD_FILE, 0, 0) == -1) {
-		log_fn("Unable to change owner of password file (%s) to superuser.");
-		return 0;
-	}
+    /* Change owner of password file to superuser. */
+    if (chown(PASSWD_FILE, 0, 0) == -1) {
+        log_fn("Unable to change owner of password file \"%s\" to superuser.",
+               PASSWD_FILE);
+        return -1;
+    }
 
-	if (chmod(PASSWD_FILE, S_IRUSR | S_IWUSR) == -1) {
-		log_fn("Unable to change password file (%s) permission bits. %s",
-			PASSWD_FILE, strerror(errno));
-		remove(PASSWD_FILE);
-		return 0;
-	}
+    /* Set its permission bits to read-write only by owner. */
+    if (chmod(PASSWD_FILE, S_IRUSR | S_IWUSR) == -1) {
+        log_fn("Unable to change password file \"%s\" permission bits. %s",
+               PASSWD_FILE, strerror(errno));
+        remove(PASSWD_FILE);
+        return -1;
+    }
 
-	return 1;
+    return 0;
 }
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	printf("Changing password for SIELD\n");
-	printf("(current) password: ");
+    char *curr_plain_txt_passwd = NULL;
+    char *new_plain_txt_passwd_1 = NULL;
+    char *new_plain_txt_passwd_2 = NULL;
+    size_t passwd_len = 0;
 
-	char *curr_plain_txt_passwd = NULL;
-	char *new_plain_txt_passwd_1 = NULL, *new_plain_txt_passwd_2 = NULL;
-	size_t passwd_len = 0;
+    printf("Changing password for SIELD\n");
+    printf("(current) password: ");
 
-	/* Get current password */
-	if (get_passwd(&curr_plain_txt_passwd, &passwd_len, stdin) == -1)
-		goto error_cleanup;
+    /* Get current password */
+    if (get_passwd(&curr_plain_txt_passwd, &passwd_len, stdin) == -1)
+        goto error_cleanup;
 
-	/* Check given password. */
-	if (! passwd_check(curr_plain_txt_passwd)) {
-		printf("\nAuthentication failure.\n");
-		printf("Password unchanged.\n");
-		goto error_cleanup;
-	}
+    /* Check given password. */
+    if (!is_passwd_correct(curr_plain_txt_passwd)) {
+        printf("\nAuthentication failure.\n");
+        printf("Password unchanged.\n");
+        goto error_cleanup;
+    }
 
-	/* Fetch new password. */
-	printf("\nEnter new password: ");
-	if (get_passwd(&new_plain_txt_passwd_1, &passwd_len, stdin) == -1)
-		goto error_cleanup;
+    /* Fetch new password. */
+    printf("\nEnter new password: ");
+    if (get_passwd(&new_plain_txt_passwd_1, &passwd_len, stdin) == -1)
+        goto error_cleanup;
 
-	/* Retype new password. */
-	printf("\nRetype new password: ");
-	if (get_passwd(&new_plain_txt_passwd_2, &passwd_len, stdin) == -1)
-		goto error_cleanup;
+    /* Retype new password. */
+    printf("\nRetype new password: ");
+    if (get_passwd(&new_plain_txt_passwd_2, &passwd_len, stdin) == -1)
+        goto error_cleanup;
 
-	/* Check if the 2 passwords match. */
-	if (strcmp(new_plain_txt_passwd_1, new_plain_txt_passwd_2)) {
-		printf("\nGiven passwords do not match.\n");
-		printf("Password unchanged.\n");
-		goto error_cleanup;
-	}
+    /* Check if the 2 passwords match. */
+    if (strcmp(new_plain_txt_passwd_1, new_plain_txt_passwd_2) != 0) {
+        printf("\nGiven passwords do not match.\n");
+        printf("Password unchanged.\n");
+        goto error_cleanup;
+    }
 
-	/* Check if new password is the same as the old one. */
-	if (! strcmp(new_plain_txt_passwd_1, curr_plain_txt_passwd)) {
-		printf("\nPassword unchanged.\n");
-	} else {
-		if (set_passwd(new_plain_txt_passwd_1)) {
-			printf("\nPassword updated successfully.\n");
-			log_fn("Password updated.");
-		} else {
-			fprintf(stderr, "\nFailed to change password.\n");
-		}
-	}
+    /* Check if new password is the same as the old one. */
+    if (strcmp(new_plain_txt_passwd_1, curr_plain_txt_passwd) == 0) {
+        printf("\nPassword unchanged.\n");
+    } else if (set_passwd(new_plain_txt_passwd_1) == 0) {
+        printf("\nPassword updated successfully.\n");
+        log_fn("Password updated.");
+    } else {
+        fprintf(stderr, "\nFailed to change password.\n");
+    }
 
-	free(curr_plain_txt_passwd);
-	free(new_plain_txt_passwd_1);
-	free(new_plain_txt_passwd_2);
+    if (curr_plain_txt_passwd) free(curr_plain_txt_passwd);
+    if (new_plain_txt_passwd_1) free(new_plain_txt_passwd_1);
+    if (new_plain_txt_passwd_2) free(new_plain_txt_passwd_2);
 
-	exit(EXIT_SUCCESS);
+    exit(EXIT_SUCCESS);
 
 error_cleanup:
-	if (curr_plain_txt_passwd) free(curr_plain_txt_passwd);
-	if (new_plain_txt_passwd_1) free(new_plain_txt_passwd_1);
-	if (new_plain_txt_passwd_2) free(new_plain_txt_passwd_2);
-	exit(EXIT_FAILURE);
+    log_fn("Failed password change event.");
+    if (curr_plain_txt_passwd) free(curr_plain_txt_passwd);
+    if (new_plain_txt_passwd_1) free(new_plain_txt_passwd_1);
+    if (new_plain_txt_passwd_2) free(new_plain_txt_passwd_2);
+    exit(EXIT_FAILURE);
 }
