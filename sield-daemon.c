e126730a9911bf799d6fca87beea432c1d5549d9
Arpan Kapoor <k.arpan26@gmail.com>
2014-07-18 01:58:27 +0530

    Convert hard tabs to spaces

--- sield-daemon.c
+++ sield-daemon.c
@@ -1,64 +1,54 @@
-#include <fcntl.h>
-#include <stdlib.h>
-#include <sys/stat.h>		/* umask(), open() */
-#include <unistd.h>		/* fork(), _exit(), setsid(), chdir() */
-
+#include <fcntl.h>          /* open() */
+#include <stdlib.h>         /* exit() */
+#include <sys/stat.h>       /* umask() */
+#include <unistd.h>         /* fork(), setsid() */
 #include "sield-daemon.h"
 
-#define BD_MAX_CLOSE 8192
-/*
- * Make the calling process a SysV daemon.
- *
- * Return -1 on error.
- */
+#define MAX_OPEN 8192          /* wild guess at number of
+                                   open file descriptors */
+
+/* Become a SysV daemon */
 int become_daemon(void)
 {
-	int maxfd, fd;
+    int fd;
+    long maxfd, i;
 
-	switch (fork()) {		/* Become backgroud process */
-	case -1: return -1;
-	case 0: break;			/* Child falls through... */
-	default: _exit(EXIT_SUCCESS);	/* while parent terminates */
-	}
+    switch (fork()) {
+        case -1: return -1;             /* error */
+        case 0: break;                  /* child process created */
+        default: exit(EXIT_FAILURE);    /* parent receives child's PID */
+    }
 
-	/* Become leader of new session */
-	if (setsid() == -1)
-		return -1;
+    /* Detach from any terminal and create an independent session. */
+    if (setsid() == -1) return -1;
 
-	switch (fork()) {
-	case -1: return -1;
-	case 0: break;
-	default: _exit(EXIT_SUCCESS);
-	}
+    /* Ensure that the daemon can never re-acquire a terminal again. */
+    switch (fork()) {
+        case -1: return -1;
+        case 0: break;
+        default: exit(EXIT_FAILURE);
+    }
 
-	/* Clear file mode creation mask */
-	umask(0);
+    /* Reset file mode creation mask. */
+    umask(0);
 
-	/* Change to root directory */
-	chdir("/");
+    /* Change current directory to root directory (/) */
+    if (chdir("/") == -1) return -1;
 
-	/* Close all open files */
-	maxfd = sysconf(_SC_OPEN_MAX);
-	if (maxfd == -1)
-		maxfd = BD_MAX_CLOSE;
+    /* Find maximum open file descriptors */
+    maxfd = sysconf(_SC_OPEN_MAX);
+    if (maxfd == -1) maxfd = MAX_OPEN;
 
-	for (fd = 0; fd < maxfd; fd++)
-		close(fd);
+    /* Close all open file descriptors */
+    for (i = 0; i < maxfd; i++) close(i);
 
+    /* Connect /dev/null to stdin, stdout, stderr */
+    close(STDIN_FILENO);
 
-	/* Reopen standard fd's to /dev/null */
-	close(STDIN_FILENO);
+    fd = open("/dev/null", O_RDWR);
+    if (fd != STDIN_FILENO) return -1;
+    if (dup2(STDIN_FILENO, STDOUT_FILENO) != STDOUT_FILENO) return -1;
+    if (dup2(STDIN_FILENO, STDERR_FILENO) != STDERR_FILENO) return -1;
 
-	fd = open("/dev/null", O_RDWR);
-
-	if (fd != STDIN_FILENO)
-		return -1;
-
-	if (dup2(STDIN_FILENO, STDOUT_FILENO) != STDOUT_FILENO)
-		return -1;
-
-	if (dup2(STDIN_FILENO, STDERR_FILENO) != STDERR_FILENO)
-		return -1;
-
-	return 0;
+    return 0;
 }
