c835606c196ef8c2ff917118e47b9826401c6823
Arpan Kapoor <k.arpan26@gmail.com>
2014-07-19 18:13:44 +0530

    Function to enumerate existing devices.

--- sield-udev-helper.c
+++ sield-udev-helper.c
@@ -1,107 +1,129 @@
-#include <errno.h>
-#include <libudev.h>
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>	/* access() */
+#include <errno.h>          /* errno */
+#include <libudev.h>        /* udev */
+#include <stdio.h>          /* fopen() */
+#include <string.h>         /* strerror() */
+#include <unistd.h>         /* access() */
 
-#include "sield-log.h"
+#include "sield-log.h"          /* log_fn() */
 #include "sield-udev-helper.h"
 
-static const char *UDEV_RULE_FILE = "/etc/udev/rules.d/999-sield-prevent-automount.rules";
+static const char *UDEV_RULE_FILE = "/etc/udev/rules.d/999-sield.rules";
+
+/*
+ * Return a udev enumeration context to list
+ * devices with given subsystem.
+ */
+struct udev_enumerate *enumerate_devices_with_subsystem(
+        struct udev *udev, const char *subsystem)
+{
+    int rt = 0;
+    struct udev_enumerate *enumerate = NULL;
+
+    enumerate = udev_enumerate_new(udev);
+    if (enumerate == NULL) {
+        log_fn("[udev] Could not initialize enumeration context");
+        return NULL;
+    }
+
+    rt = udev_enumerate_add_match_subsystem(enumerate, subsystem);
+    if (rt != 0) {
+        log_fn("Failed to setup enumeration filter.");
+        return NULL;
+    }
+
+    rt = udev_enumerate_scan_devices(enumerate);
+    if (rt != 0) {
+        log_fn("Failed to scan enumeration devices.");
+        return NULL;
+    }
+
+    return enumerate;
+}
 
 /*
  * Return a listening udev_monitor with given
  * event source, subsystem and device type.
  */
 struct udev_monitor *monitor_device_with_subsystem_devtype(
-	struct udev *udev, const char *event_source,
-	const char *subsystem, const char *devtype)
+        struct udev *udev, const char *event_source,
+        const char *subsystem, const char *devtype)
 {
-	struct udev_monitor *monitor = udev_monitor_new_from_netlink(
-					udev, event_source);
-	if (! monitor) {
-		log_fn("Failed to setup a new udev_monitor.");
-		return NULL;
-	}
+    int rt = 0;
+    struct udev_monitor *monitor = NULL;
 
-	int rt;
-	rt = udev_monitor_filter_add_match_subsystem_devtype(
-		monitor, subsystem, devtype);
-	if (rt != 0) {
-		log_fn("Failed to setup monitor filter.");
-		return NULL;
-	}
+    monitor = udev_monitor_new_from_netlink(udev, event_source);
+    if (monitor == NULL) {
+        log_fn("Failed to setup a new udev_monitor.");
+        return NULL;
+    }
 
-	rt = udev_monitor_enable_receiving(monitor);
-	if (rt != 0) {
-		log_fn("Failed to bind udev_monitor to event source.");
-		return NULL;
-	}
+    rt = udev_monitor_filter_add_match_subsystem_devtype(
+            monitor, subsystem, devtype);
+    if (rt != 0) {
+        log_fn("Failed to setup monitor filter.");
+        return NULL;
+    }
 
-	return monitor;
+    rt = udev_monitor_enable_receiving(monitor);
+    if (rt != 0) {
+        log_fn("Failed to bind udev_monitor to event source.");
+        return NULL;
+    }
+
+    return monitor;
 }
 
 /* Return a udev_device with given action, else return NULL */
 struct udev_device *receive_device_with_action(
-	struct udev_monitor *monitor, const char *action)
+        struct udev_monitor *monitor, const char *action)
 {
-	/* udev_monitor_receive_device is NONBLOCKING */
-	struct udev_device *device = udev_monitor_receive_device(monitor);
-	if (device) {
-		const char *actual_action = udev_device_get_action(device);
-		if (! strcmp(actual_action, action)) return device;
-	}
+    /* udev_monitor_receive_device is NONBLOCKING */
+    struct udev_device *device = udev_monitor_receive_device(monitor);
 
-	return NULL;
+    if (device != NULL) {
+        const char *actual_action = udev_device_get_action(device);
+        if (strcmp(actual_action, action) == 0) return device;
+    }
+
+    return NULL;
 }
 
-static int udev_rule_file_exists(void)
-{
-	return access(UDEV_RULE_FILE, F_OK) == 0;
-}
-
-/*
- * Delete udev rule file.
- *
- * Return 1 on success, 0 on error.
- */
+/* Delete udev rule file. */
 int delete_udev_rule(void)
 {
-	if (! udev_rule_file_exists()) return 1;
+    /* File doesn't exist */
+    if (access(UDEV_RULE_FILE, F_OK) == -1) return 0;
 
-	if (remove(UDEV_RULE_FILE) == -1) {
-		log_fn("Unable to delete udev rule file: %s", strerror(errno));
-		return 0;
-	}
+    if (remove(UDEV_RULE_FILE) == -1) {
+        log_fn("Unable to delete udev rule file: %s", strerror(errno));
+        return -1;
+    }
 
-	return 1;
+    return 0;
 }
 
-/*
- * Write the udev rule for sield.
- *
- * Return 1 on success, else return 0.
- */
+/* Write the udev rule to prevent automount. */
 int write_udev_rule(void)
 {
-	/* Rule file already exists */
-	if (udev_rule_file_exists()) return 1;
+    const char *rule =
+        "ACTION==\"add|change\", SUBSYSTEM==\"block\","
+        "SUBSYSTEMS==\"usb\", ENV{UDISKS_PRESENTATION_HIDE}=\"1\","
+        "ENV{UDISKS_PRESENTATION_NOPOLICY}=\"1\","
+        "ENV{UDISKS_AUTOMOUNT_HINT}=\"never\","
+        "ENV{UDISKS_IGNORE}=\"1\", ENV{UDISKS_AUTO}=\"0\"";
+    FILE *fp = NULL;
 
-	FILE *udev_fp = fopen(UDEV_RULE_FILE, "w");
-	if (! udev_fp) {
-		log_fn("Can't open udev rule file for writing: %s", strerror(errno));
-		return 0;
-	}
+    /* Rule file already exists */
+    if (access(UDEV_RULE_FILE, F_OK) == 0) return 0;
 
-	const char *rule =
-		"ACTION==\"add|change\", SUBSYSTEM==\"block\","
-		"SUBSYSTEMS==\"usb\", ENV{UDISKS_PRESENTATION_HIDE}=\"1\","
-		"ENV{UDISKS_PRESENTATION_NOPOLICY}=\"1\","
-		"ENV{UDISKS_AUTOMOUNT_HINT}=\"never\","
-		"ENV{UDISKS_IGNORE}=\"1\", ENV{UDISKS_AUTO}=\"0\"";
+    fp = fopen(UDEV_RULE_FILE, "w");
+    if (fp == NULL) {
+        log_fn("Can't open udev rule file for writing: %s", strerror(errno));
+        return -1;
+    }
 
-	fprintf(udev_fp, "%s\n", rule);
+    fprintf(fp, "%s\n", rule);
 
-	fclose(udev_fp);
-	return 1;
+    fclose(fp);
+    return 0;
 }
--- sield-udev-helper.h
+++ sield-udev-helper.h
@@ -3,16 +3,16 @@
 
 #include <libudev.h>
 
-/*
- * udev helper functions
- */
+/* udev helper functions */
+struct udev_enumerate *enumerate_devices_with_subsystem(
+        struct udev *udev, const char *subsystem)
 
 struct udev_monitor *monitor_device_with_subsystem_devtype(
-	struct udev *udev, const char *event_source,
-	const char *subsystem, const char *devtype);
+        struct udev *udev, const char *event_source,
+        const char *subsystem, const char *devtype);
 
 struct udev_device *receive_device_with_action(
-	struct udev_monitor *monitor, const char *action);
+        struct udev_monitor *monitor, const char *action);
 
 int delete_udev_rule(void);
 int write_udev_rule(void);
