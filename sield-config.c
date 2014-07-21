26064ebbcfaaf8a2da00a1fe4b8ea4c8c8bf4232
Arpan Kapoor <k.arpan26@gmail.com>
2014-07-21 12:01:20 +0530

    tabs -> spaces

--- sield-config.c
+++ sield-config.c
@@ -1,53 +1,50 @@
-#define _GNU_SOURCE		/* getline() */
-#define _BSD_SOURCE		/* strsep() */
-#include <errno.h>		/* strerror() */
-#include <stdio.h>
-#include <stdlib.h>		/* free() */
-#include <string.h>		/* strsep(), strcmp() */
+#define _GNU_SOURCE     /* getline() */
+#define _BSD_SOURCE     /* strsep() */
+#include <errno.h>      /* errno */
+#include <stdio.h>      /* fopen() */
+#include <stdlib.h>     /* free() */
+#include <string.h>     /* strsep(), strcmp() */
 
 #include "sield-config.h"
-#include "sield-log.h"
+#include "sield-log.h"  /* log_fn() */
 
-static const char *config_file = "/etc/sield.conf";
+static const char *CONFIG_FILE = "/etc/sield.conf";
 
 static char *strip_whitespace(char *str);
-static char *seperate_line_into_name_value(
-		char **line, const char *delim);
+static char *seperate_line_into_name_value(char **line, const char *delim);
 static char *get_sield_attr_base(const char *name, int log);
-char *get_sield_attr(const char *name);
-char *get_sield_attr_no_log(const char *name);
-long int get_sield_attr_int(const char *name);
 
 /*
- * Return pointer to string without trailing or whitespace at
+ * Return pointer to string without trailing whitespace or whitespace at
  * the beginning of the string.
  *
- * Return NULL if string is NULL or
- * entire string consists of whitespace.
+ * Return NULL if string is NULL or entire string consists of whitespace.
  */
 static char *strip_whitespace(char *str)
 {
-	if (! str) return NULL;
+    char *ret = NULL;
 
-	/* Remove whitespace from the beginning of the string. */
-	while (*str != '\0' && (*str == ' ' || *str == '\t')) str++;
+    if (str == NULL) return NULL;
 
-	/* Entire line is whitespace */
-	if (*str == '\0') return NULL;
+    /* Remove whitespace from the beginning of the string. */
+    while (*str != '\0' && (*str == ' ' || *str == '\t')) str++;
 
-	/* Return string begins here. */
-	char *ret = str;
+    /* Entire line is whitespace */
+    if (*str == '\0') return NULL;
 
-	/* Move to the end of the string. */
-	while (*str != '\0') str++;
-	str--;
+    /* First non-whitespace character */
+    ret = str;
 
-	/* Move backwards until a non-whitespace character is found. */
-	while (*str == ' ' || *str == '\t') str--;
-	str++;
+    /* Move to the end of the string */
+    while (*str != '\0') str++;
+    str--;
 
-	*str = '\0';
-	return ret;
+    /* Move backwards until a non-whitespace character is found. */
+    while (*str == ' ' || *str == '\t') str--;
+    str++;
+
+    *str = '\0';
+    return ret;
 }
 
 /*
@@ -55,26 +52,23 @@ static char *strip_whitespace(char *str)
  * remove any trailing or beginning whitespace from both of them.
  *
  * Return attribute name.
- *
  * The argument given will point to the attribute value.
  */
-static char *seperate_line_into_name_value(
-	char **line, const char *delim)
+static char *seperate_line_into_name_value(char **line, const char *delim)
 {
-	char *name = strsep(line, delim);
-	char *value = strsep(line, "\n");
+    char *name = strsep(line, delim);
+    char *value = strsep(line, "\n");
 
-	name = strip_whitespace(name);
-	value = strip_whitespace(value);
+    name = strip_whitespace(name);
+    value = strip_whitespace(value);
 
-	*line = value;
+    *line = value;
 
-	return name;
+    return name;
 }
 
 /*
- * Return attribute with given name if present,
- * else return NULL.
+ * Return attribute with given name if present, else return NULL.
  *
  * Log any errors if "log" is non-zero.
  *
@@ -83,83 +77,81 @@ static char *seperate_line_into_name_value(
  */
 static char *get_sield_attr_base(const char *name, int log)
 {
-	FILE *config_fp = fopen(config_file, "r");
-	if (! config_fp) {
-		if (log) {
-			log_fn("fopen: %s", strerror(errno));
-			log_fn("Unable to open config file for reading.");
-		}
-		return NULL;
-	}
+    size_t len = 0;
+    const char *delim = "=";
+    char *value = NULL;
+    char *line = NULL;
+    FILE *fp = NULL;
 
-	const char *delim = "=";
-	char *value = NULL;
-	char *line = NULL;
-	size_t len = 0;
+    fp = fopen(CONFIG_FILE, "r");
+    if (fp == NULL) {
+        if (log != 0) log_fn("fopen: %s: %s", CONFIG_FILE, strerror(errno));
+        return NULL;
+    }
 
-	while (! value
-		&& getline(&line, &len, config_fp) != -1) {
+    while (value == NULL
+            && getline(&line, &len, fp) != -1) {
 
-		/* Skip empty lines and those starting with # (comments) */
-		if (line[0] == '#' || line[0] == '\n') continue;
+        char *line_begin = line;
+        char *name_t = NULL;
+        char *value_t = NULL;
 
-		char *line_ptr = line;
+        /* Skip empty lines and those starting with # (comments) */
+        if (line[0] == '#' || line[0] == '\n') continue;
 
-		char *name_t = seperate_line_into_name_value(&line_ptr, delim);
-		char *value_t = line_ptr;
+        name_t = seperate_line_into_name_value(&line_begin, delim);
+        value_t = line_begin;
 
-		/* strdup() should have a non-null argument. */
-		if (! strcmp(name_t, name) && value_t) value = strdup(value_t);
-	}
+        /* strdup() should have a non-null argument. */
+        if (strcmp(name_t, name) == 0 && value_t != NULL)
+            value = strdup(value_t);
+    }
 
-	if (! value && log) log_fn("Cannot find configuration for \"%s\".", name);
+    if (value == NULL && log != 0)
+        log_fn("Cannot find configuration for '%s'.", name);
 
-	/* Clean up */
-	if (line) free(line);
-	fclose(config_fp);
+    /* Clean up */
+    if (line) free(line);
+    fclose(fp);
 
-	return value;
+    return value;
 }
 
-/*
- * Get attribute value without logging.
- */
+/* Get attribute value without logging. */
 char *get_sield_attr_no_log(const char *name)
 {
-	return get_sield_attr_base(name, 0);
+    return get_sield_attr_base(name, 0);
 }
 
-/*
- * Logging is turned on.
- */
+/* Logging is turned on */
 char *get_sield_attr(const char *name)
 {
-	return get_sield_attr_base(name, 1);
+    return get_sield_attr_base(name, 1);
 }
 
-/*
- * Convert value of given attribute to int.
- */
+/* Convert value of given attribute to an integer */
 long int get_sield_attr_int(const char *name)
 {
-	char *value_str = get_sield_attr(name);
+    long int ret = -1;
+    char *value_str = NULL;
+    char *endptr = NULL;
 
-	/* Return -1 if not present. */
-	if (! value_str) return -1;
+    value_str = get_sield_attr(name);
 
-	char *endptr;
+    /* Return -1 if not present. */
+    if (value_str == NULL) return -1;
 
-	/* Convert to integer. */
-	long int value = strtol(value_str, &endptr, 10);
+    /* Convert to integer. */
+    ret = strtol(value_str, &endptr, 10);
 
-	/* The configuration is not written properly. */
-	if (*endptr != '\0') {
-		log_fn("Non-integer characters in the config \"%s\"", name);
-		value = -1;
-	}
+    /* The configuration is not written properly. */
+    if (*endptr != '\0') {
+        log_fn("Non-integer characters in the config \"%s\"", name);
+        ret = -1;
+    }
 
-	free(value_str);
-	return value;
+    free(value_str);
+    return ret;
 }
 
 /* Return -1 if attribute value is set to anything other than 0 or 1 */
